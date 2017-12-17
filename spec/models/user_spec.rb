0ee0d5fd8de444bf5b08f0312eb5c2e542704c4d
Igor Melão <igormelao@gmail.com>
2017-12-16 22:41:07 -0300

    Create user
    Create user using gem devise following TDD steps below:
    
      * create model test for user to test some fields like:
         - DB structure
             expected to have db column id with type integer
             expected to have db column name with type string
             expected to have db column email with type string
             expected to have db column created_at with type datetime
             expected to have db column updated_at with type datetime
    
         - Validations
            expected to validate presence of email
            expected to validate presence of password
    
      * running follow command to run tests and see they failures
        -docker-compose run --rm app bundle exec rspec
    
      * create a user devise running commando below
        - docker-compose run --rm app bundle exec rails g devise User
    
      * create migration to add name into User running command below
        - docker-compose run --rm app bundle exec rails g migration
          addNameToUser name
    
      * running follow command to run tests and see they pass successfully
        -docker-compose run --rm app bundle exec rspec
    
    As a result, this application it's ready to create, update and delete
    users as too ready to use authentication through devise.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "db structure" do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
-
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE_)
+require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'shoulda/matchers'
@@ -15,9 +14,9 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Warden::Test::Helpers
 end
